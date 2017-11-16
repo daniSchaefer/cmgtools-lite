fead56bad7ab726768e3a85125ad78b13ff80666
Daniela Schaefer <daniela.schafer@cern.ch>
2017-11-16 17:32:34 +0100

    tested something in kernel method

--- VVResonances/scripts/vvMake2DTemplatesTest.py
+++ VVResonances/scripts/vvMake2DTemplatesTest.py
@@ -48,48 +48,48 @@ def Gaussian2D(varx, vary,var,hscalex,hscaley,hresx, hresy, varw, minX,maxX,minY
     resx=hresx.Interpolate(var)*varx;
     resy=hresy.Interpolate(var)*vary;
     
-    #
-    #print str(resx)+'   '+str(resy)
-    
-    
-    gaussian = ROOT.TF2("gauss2D","exp(-0.5*(x-[0])*(x-[0])/([1]*[1])-0.5*(y-[2])*(y-[2])/([3]*[3]))",minX,maxX,minY,maxY)
-    gaussian.SetParameter(0,scalex)
-    gaussian.SetParameter(1,resx)
-    gaussian.SetParameter(2,scaley)
-    gaussian.SetParameter(3,resy)
-    
     if resx < 1e-6 or resy <1e-6:
         print 'attention for some reason resolution becomes very small!'
         print str(scalex)+'   '+str(scaley)
         print str(resx)+'   '+str(resy)
         
+    
+    #print str(resx)+'   '+str(resy)
+    
+    
+    #gaussian = ROOT.TF2("gauss2D","exp(-0.5*(x-[0])*(x-[0])/([1]*[1])-0.5*(y-[2])*(y-[2])/([3]*[3]))",minX,maxX,minY,maxY)
+    #gaussian.SetParameter(0,scalex)
+    #gaussian.SetParameter(1,resx)
+    #gaussian.SetParameter(2,scaley)
+    #gaussian.SetParameter(3,resy)
+    
         
-    #print 'scale '+str(scalex)+'   '+str(scaley)
-    #print 'resolution '+str(resx)+'   '+str(resy)
-    #print gaussian.Eval(varx,vary)
+    ##print 'scale '+str(scalex)+'   '+str(scaley)
+    ##print 'resolution '+str(resx)+'   '+str(resy)
+    ##print gaussian.Eval(varx,vary)
     
-    #c= ROOT.TCanvas('test','test',400,400)
-    #gaussian.Draw()
-    #c.SaveAs('test.pdf')
-    norm = gaussian.Integral(minX,maxX,minY,maxY)
-    mVV=varx
-    mjet=vary
-    mVVmin= mVV*0.8
-    mVVmax= mVV*1.2
-    if options.minx < mVVmin:
-      mVVmin= options.minx
-    if options.maxx > mVVmax:
-      mVVmax= options.maxx
+    ##c= ROOT.TCanvas('test','test',400,400)
+    ##gaussian.Draw()
+    ##c.SaveAs('test.pdf')
+    #norm = gaussian.Integral(minX,maxX,minY,maxY)
+    #mVV=varx
+    #mjet=vary
+    #mVVmin= mVV*0.8
+    #mVVmax= mVV*1.2
+    #if options.minx < mVVmin:
+      #mVVmin= options.minx
+    #if options.maxx > mVVmax:
+      #mVVmax= options.maxx
       
-    mjetmin= mjet*0.8
-    mjetmax= mjet*1.2
-    if options.miny < mjetmin:
-        mjetmin= options.miny
-    if options.maxy > mjetmax:
-        mjetmax= options.maxy
-    norm = gaussian.Integral(mVVmin,mVVmax,mjetmin,mjetmax)
+    #mjetmin= mjet*0.8
+    #mjetmax= mjet*1.2
+    #if options.miny < mjetmin:
+        #mjetmin= options.miny
+    #if options.maxy > mjetmax:
+        #mjetmax= options.maxy
+    #norm = gaussian.Integral(mVVmin,mVVmax,mjetmin,mjetmax)
     
-    #print 'unnormalised gaussian '+str(norm)
+    ##print 'unnormalised gaussian '+str(norm)
     #print norm
     #print varw
     #print varw/norm
@@ -99,6 +99,13 @@ def Gaussian2D(varx, vary,var,hscalex,hscaley,hresx, hresy, varw, minX,maxX,minY
     Ngaussian.SetParameter(1,resx)
     Ngaussian.SetParameter(2,scaley)
     Ngaussian.SetParameter(3,resy)
+    #if vary > 470:
+        #Ngaussian.SetParameter(3,resy*1.2)
+        #Ngaussian.SetParameter(1,resx*1.2)
+    #if varx > 3500:
+        #Ngaussian.SetParameter(3,resy*1.2)
+        #Ngaussian.SetParameter(1,resx*1.2)
+    
     #Ngaussian.SetParameter(4,1./float(norm))
     #print 'normalised gaussian '+str(Ngaussian.Integral(minX,maxX,minY,maxY))
     #print 'result normalised '+str(Ngaussian.Eval(varx,vary))
@@ -109,15 +116,17 @@ def Gaussian2D(varx, vary,var,hscalex,hscaley,hresx, hresy, varw, minX,maxX,minY
 def FillSmoothed(hist,gauss,w,mVV,mjet):
   Nx = hist.GetNbinsX()
   Ny = hist.GetNbinsY()
-  mVVmin= mVV*0.7
-  mVVmax= mVV*1.4
+  mVVmin= mVV*0.2
+  mVVmax= mVV*1.2
+  mjetmin= mjet*0.2
+  mjetmax= mjet*1.2
+  
+  
   if options.minx > mVVmin:
       mVVmin= options.minx
   if options.maxx < mVVmax:
       mVVmax= options.maxx
       
-  mjetmin= mjet*0.6
-  mjetmax= mjet*1.5
   if options.miny > mjetmin:
       mjetmin= options.miny
   if options.maxy < mjetmax:
@@ -125,19 +134,17 @@ def FillSmoothed(hist,gauss,w,mVV,mjet):
   norm = 1 #gauss.Integral(mVVmin,mVVmax,mjetmin,mjetmax)
 
   for i in range(1,Ny+1):
+      vary   = hist.GetYaxis().GetBinCenter(i)
+      if vary < mjetmin:
+              continue
+      if vary> mjetmax:
+              continue
       for j in range(1,Nx+1):
           varx   = hist.GetXaxis().GetBinCenter(j)
-          vary   = hist.GetYaxis().GetBinCenter(i)
-          #print 'bin '+str(varx)+' '+str(vary)
           if varx < mVVmin:
-              continue
-          if vary < mjetmin:
               continue
           if varx> mVVmax:
               continue
-          if vary> mjetmax:
-              continue
-          #print gauss.Eval(varx,vary)
           v = gauss.Eval(varx,vary)/norm
           v=v*w
           if v <= 1e-30:
@@ -150,6 +157,40 @@ def FillSmoothed(hist,gauss,w,mVV,mjet):
           old = hist.GetBinContent(j,i)
           hist.SetBinContent(j,i,v+old)
   return hist
+
+def FillSmoothedTest2(i,j,gauss,w, hist):
+    vary   = hist.GetYaxis().GetBinCenter(i)
+    varx   = hist.GetXaxis().GetBinCenter(j)
+    v = gauss.Eval(varx,vary)
+    v=v*w
+    old = hist.GetBinContent(j,i)
+    hist.SetBinContent(j,i,v+old)
+    return hist
+
+def conditional(hist):
+    for i in range(1,hist.GetNbinsY()+1):
+        proj=hist.ProjectionX("q",i,i)
+        integral=proj.Integral()
+        if integral==0.0:
+            print 'SLICE WITH NO EVENTS!!!!!!!!',hist.GetName()
+            continue
+        for j in range(1,hist.GetNbinsX()+1):
+            hist.SetBinContent(j,i,hist.GetBinContent(j,i)/integral)
+
+
+
+def expandHisto(histo,options):
+    #histogram=ROOT.TH2F(histo.GetName(),"histo",options.binsx,options.minx,options.maxx,options.binsy,options.miny,options.maxy)
+    histogram = ROOT.TH2F(histo.GetName(),"histo",int(options.binsx),options.minx,options.maxx,options.binsy,options.miny,options.maxy)
+    for i in range(1,histo.GetNbinsX()+1):
+        proje = histo.ProjectionY("q",i,i)
+        graph=ROOT.TGraph(proje)
+        for j in range(1,histogram.GetNbinsY()+1):
+            x=histogram.GetYaxis().GetBinCenter(j)
+            bin=histogram.GetBin(i,j)
+            histogram.SetBinContent(bin,graph.Eval(x,0,"S"))
+    return histogram
+
 
 sampleTypes=options.samples.split(',')
 print "Creating datasets for samples: " ,sampleTypes
@@ -211,7 +252,7 @@ histogram = ROOT.TH2F("histo_nominal","histo_nominal",options.binsx,options.minx
 histograms=[
     histogram
 ]
-histI2D =dataPlotters[0].drawTH2("jj_l1_gen_softDrop_mass:jj_gen_partialMass",options.cut,"1",options.binsx+2,options.minx-100,options.maxx+100,options.binsy,options.miny,options.maxy,"M_{qV} mass","GeV","Softdrop mass","GeV","COLZ" )
+histI2D =dataPlotters[0].drawTH2("jj_l1_gen_softDrop_mass:jj_gen_partialMass",options.cut,"1",options.binsx+3,options.minx-31.25*2,options.maxx+31.25,options.binsy,options.miny,options.maxy,"M_{qV} mass","GeV","Softdrop mass","GeV","COLZ" )
 for plotter,plotterNW in zip(dataPlotters,dataPlottersNW):
 
  #if plotter.filename != 'QCD_Pt-15to7000': continue
@@ -224,8 +265,8 @@ for plotter,plotterNW in zip(dataPlotters,dataPlottersNW):
   #histI=plotter.drawTH1(variables[0],options.cut,"1",1,0,1000000000)
   #norm=histI.Integral()
   #y:x
-  histI2D.Add(plotter.drawTH2("jj_l1_gen_softDrop_mass:jj_gen_partialMass",options.cut,"1",options.binsx+2,options.minx-100,options.maxx+100,options.binsy,options.miny,options.maxy,"M_{qV} mass","GeV","Softdrop mass","GeV","COLZ" ))
-
+  histI2D.Add(plotter.drawTH2("jj_l1_gen_softDrop_mass:jj_gen_partialMass",options.cut,"1",options.binsx+3,options.minx-31.25*2,options.maxx+31.25,options.binsy,options.miny,options.maxy,"M_{qV} mass","GeV","Softdrop mass","GeV","COLZ" ))
+  #conditional(histI2D)
   #print " - Creating dataset - "
   #dataset=plotterNW.makeDataSet(varsDataSet,options.cut,maxEvents)
 
@@ -238,6 +279,8 @@ Ny = histI2D.GetNbinsY()
 print " minVV "+str(options.minx)+" max MVV "+str(options.maxx)
 print " min mjet "+str(options.miny) + " Max mjet "+str(options.maxy) 
 for i in range(1,Ny+1):
+      if i%10 == 0:
+          print 'doing bin '+str(varx)+' '+str(vary)
       for j in range(1,Nx+1):
           w = histI2D.GetBinContent(j,i)
           if w==0:
@@ -247,19 +290,9 @@ for i in range(1,Ny+1):
           gauss  = Gaussian2D(varx, vary,varx,scale_x,scale_y,res_x, res_y, w,options.minx,options.maxx,options.miny,options.maxy)
           #print 'doing bin '+str(varx)+' '+str(vary)
           FillSmoothed(histogram,gauss,w*1000000,varx,vary)
-  #histTMP=ROOT.TH2F("histoTMP","histo",len(binsx)-1,array('f',binsx),len(binsy)-1,array('f',binsy))
-  #if not(options.usegenmass): 
-   #datamaker=ROOT.cmg.GaussianSumTemplateMaker(dataset,variables[0],variables[1],'jj_l1_gen_pt',scale_x,scale_y,res_x,res_y,histTMP)
-  #else: datamaker=ROOT.cmg.GaussianSumTemplateMaker(dataset,variables[0],variables[1],'jj_l1_gen_softDrop_mass',scale_x,scale_y,res_x,res_y,histTMP)
+          #FillSmoothedTest2(i,j,gauss,w,histogram)
+ 
 
-  #if histTMP.Integral()>0:
-   #histTMP.Scale(histI2D.Integral()/histTMP.Integral())
-   #histogram.Add(histTMP)
-   #mjet_mvv_nominal.Add(histI2D)
-   
-  ##histI.Delete()  	  
-  #histTMP.Delete()
-  
 f=ROOT.TFile(options.output,"RECREATE")
 print "Finished producing histograms! Saving to" ,options.output
 histI2D.SetName('genHisto')
@@ -267,4 +300,5 @@ histI2D.Write()
 print histograms
 for hist in histograms:
  print "Working on histogram " ,hist.GetName()
- hist.Write()
+ expanded = expandHisto(hist,options) 
+ expanded.Write()
