a21077588a15681d547f522c5086131587b0007a
dschafer <dschafer@cern.ch>
2020-09-09 09:34:58 +0200

    changed to cuts class in jecuncertainties_2017 -> attention still only does VV_HPHP and VV_HPLP!

--- VVResonances/interactive/JECUncertainties_2017.py
+++ VVResonances/interactive/JECUncertainties_2017.py
@@ -1,35 +1,26 @@
 import ROOT
 import os,sys
 import numpy
+import cuts
 
-HPSF = 0.948
-LPSF = 1.057
+
+ 
+ctx  = cuts.cuts("init_VV_VH.json","2017","random_dijetbins",False) 
+
+HPSF = ctx.HPSF_vtag*ctx.HPSF_vtag
+LPSF = ctx.HPSF_ctag*ctx.LPSF_vtag
+
+HCALbinsMVV=" --binsMVV "+ctx.HCALbinsMVV
     
-
-HCALbinsMVV=" --binsMVV 1,3,6,10,16,23,31,40,50,61,74,88,103,119,137,156,176,197,220,244,270,296,325,354,386,419,453,489,526,565,606,649,693,740,788,838,890,944,1000,1058,1118,1181,1246,1313,1383,1455,1530,1607,1687,1770,1856,1945,2037,2132,2231,2332,2438,2546,2659,2775,2895,3019,3147,3279,3416,3558,3704,3854,4010,4171,4337,4509,4686,4869,5058,5253,5455,5663,5877,6099,6328,6564,6808"
-    
-
- 	
-cat={}
-
-# For retuned DDT tau 21, use this
-cat['HP1'] = '(jj_l1_tau2/jj_l1_tau1+(0.082*TMath::Log((jj_l1_softDrop_mass*jj_l1_softDrop_mass)/jj_l1_pt)))<0.57'
-cat['HP2'] = '(jj_l2_tau2/jj_l2_tau1+(0.082*TMath::Log((jj_l2_softDrop_mass*jj_l2_softDrop_mass)/jj_l2_pt)))<0.57'
-cat['LP1'] = '(jj_l1_tau2/jj_l1_tau1+(0.082*TMath::Log((jj_l1_softDrop_mass*jj_l1_softDrop_mass)/jj_l1_pt)))>0.57&&(jj_l1_tau2/jj_l1_tau1+(0.082*TMath::Log((jj_l1_softDrop_mass*jj_l1_softDrop_mass)/jj_l1_pt)))<0.98'
-cat['LP2'] = '(jj_l2_tau2/jj_l2_tau1+(0.082*TMath::Log((jj_l2_softDrop_mass*jj_l2_softDrop_mass)/jj_l2_pt)))>0.57&&(jj_l2_tau2/jj_l2_tau1+(0.082*TMath::Log((jj_l2_softDrop_mass*jj_l2_softDrop_mass)/jj_l2_pt)))<0.98'
-cat['NP1'] = '(jj_l1_tau2/jj_l1_tau1+(0.082*TMath::Log((jj_l1_softDrop_mass*jj_l1_softDrop_mass)/jj_l1_pt)))>0.98'
-cat['NP2'] = '(jj_l2_tau2/jj_l2_tau1+(0.082*TMath::Log((jj_l2_softDrop_mass*jj_l2_softDrop_mass)/jj_l2_pt)))>0.98'
 
 cuts={}
 
 
-cuts['common'] = '((HLT_JJ)*(run>500) + (run<500))*(njj>0&&jj_LV_mass>700&&abs(jj_l1_eta-jj_l2_eta)<1.3&&jj_l1_softDrop_mass>0.&&jj_l2_softDrop_mass>0.)'
-cuts['metfilters'] = "(((run>2000*Flag_eeBadScFilter)+(run<2000))&&Flag_goodVertices&&Flag_globalTightHalo2016Filter&&Flag_HBHENoiseFilter&&Flag_HBHENoiseIsoFilter&&Flag_eeBadScFilter&&Flag_EcalDeadCellTriggerPrimitiveFilter&&Flag_BadPFMuonFilter&&Flag_BadChargedCandidateFilter&&Flag_ecalBadCalibFilter)"
+cuts['common'] = ctx.cuts['common']
  
 
-cuts['HPHP'] = '('+cat['HP1']+'&&'+cat['HP2']+')'
-cuts['LPLP'] = '('+cat['LP1']+'&&'+cat['LP2']+')'
-cuts['HPLP'] = '(('+cat['HP1']+'&&'+cat['LP2']+')||('+cat['LP1']+'&&'+cat['HP2']+'))'
+cuts['HPHP'] = ctx.cuts['VV_HPHP']
+cuts['HPLP'] = ctx.cuts['VV_HPLP']
 cuts['NP'] = '1'
 
 
@@ -49,21 +40,19 @@ BRWZ=1.*0.001*0.6991*0.676
 
 
   
-minMJ=55.0
-maxMJ=215.0
-minMVV =1000
-maxMVV=5500
+minMJ=ctx.minMJ
+maxMJ=ctx.maxMJ
+minMVV =ctx.minMVV
+maxMVV=ctx.minMVV
 
-binsMJ=80
-binsMVV=100
-binsMVV = 36
+binsMJ=ctx.binsMJ
+binsMVV = ctx.binsMVV
     
 
-cuts['acceptance']= "(jj_LV_mass>{minMVV}&&jj_LV_mass<{maxMVV}&&jj_l1_softDrop_mass>{minMJ}&&jj_l1_softDrop_mass<{maxMJ}&&jj_l2_softDrop_mass>{minMJ}&&jj_l2_softDrop_mass<{maxMJ})".format(minMVV=minMVV,maxMVV=maxMVV,minMJ=minMJ,maxMJ=maxMJ)
-cuts['acceptanceGEN']='(jj_l1_gen_softDrop_mass>20&&jj_l2_gen_softDrop_mass>20&&jj_l1_gen_softDrop_mass<300&&jj_l2_gen_softDrop_mass<300&&jj_gen_partialMass>400)'
+cuts['acceptance']= ctx.cuts['acceptance']
+cuts['acceptanceGEN']=ctx.cuts['acceptanceGEN']
 
-cuts['acceptanceMJ']= "(jj_l1_softDrop_mass>{minMJ}&&jj_l1_softDrop_mass<{maxMJ}&&jj_l2_softDrop_mass>{minMJ}&&jj_l2_softDrop_mass<{maxMJ})".format(minMJ=minMJ,maxMJ=maxMJ) 
-
+cuts['acceptanceMJ']= ctx.cuts['acceptanceMJ']
 
 
 def makeJECShapesMVV(filename,template):
